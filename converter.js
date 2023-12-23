f53fef4eabd7d42b34065d96d5a353395b7a51e4
Draggie <75524491+Draggie306@users.noreply.github.com>
2023-12-23 19:48:42 +0000

    Initial commit
    Add Python, HTML + JS files, and the README

---
+++ converter.js
@@ -0,0 +1,88 @@
+// Last modified: 23/12/2023 19:28 by Draggie306
+// Converts Kaspersky Password Manager export to Chromium Password Manager CSV export
+
+// Permission is granted to anyone to use this software for any purpose, including commercial applications, providing that the following conditions are met:
+// 1. Give appropriate credit, provide a link to the original source (https://github.com/Draggie306/kaspersky-to-csv) and indicate if changes were made.
+// 2. Do not use this software for illegal purposes.
+// 3. If you find a bug, report it to me so I can fix it.
+// 4. If you make any changes, you must release them under the same license.
+// 5. Do not claim this software as your own.
+// 6. Have fun!
+
+// on DOM load
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('Status').innerHTML = 'Status: Ready';
+
+    // Ensure the element exists before attaching event listener
+    var convertButton = document.getElementById('convertButton');
+    if (convertButton) {
+        convertButton.addEventListener('click', function() {
+        var accountCount = 0;
+        var countOfAccounts = document.getElementById('countOfAccounts');
+        var fileInput = document.getElementById('fileInput');
+        var file = fileInput.files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var lines = e.target.result.split('\n');
+            var websites = [];
+            var i = 0;
+            while (i < lines.length) {
+                var line = lines[i].trim();
+                if (line.includes('Websites')) {
+                    i++;
+                    while (i < lines.length && !lines[i].includes('Applications')) {
+                        if (lines[i].includes('---')) {
+                            i++;
+                            continue;
+                        }
+                        var record = {};
+                        while (i < lines.length && !lines[i].includes('---')) {
+                            // holy this is ugly
+                            if (lines[i].includes('Website name:')) {
+                                let value = lines[i].split(':', 2)[1].trim();
+                                record['Website name'] = `"${value.replace(/"/g, '""')}"`;
+                            } else if (lines[i].includes('Website URL:')) {
+                                record['Website URL'] = `"${lines[i].replace('Website URL: ', '').trim()}"`;
+                            } else if (lines[i].includes('Login:')) {
+                                let value = lines[i].split(':', 2)[1].trim();
+                                record['Login'] = `"${value.replace(/"/g, '""')}"`;
+                            } else if (lines[i].includes('Password:')) {
+                                let value = lines[i].split(':').slice(1).join(':').trim();
+                                record['Password'] = `"${value.replace(/"/g, '""')}"`;
+                            } else if (lines[i].includes('Application:')) {
+                                let value = lines[i].split(':', 2)[1].trim();
+                                record['Application'] = `"${value.replace(/"/g, '""')}"`;
+                            } else if (lines[i].includes('Comment:')) {
+                                let value = lines[i].split(':', 2)[1].trim();
+                                record['Comment'] = `"${value.replace(/"/g, '""')}"`;
+                            }
+                            i++;
+                        }
+                        accountCount++;
+                        countOfAccounts.innerHTML = `Accounts found: ${accountCount}`;
+                        websites.push(record);
+                    }
+                }
+                i++;
+            }
+            var csvContent = 'name,url,username,password,note,isApplication\n';
+            for (var website of websites) {
+                var note = website['Comment'] || '';
+                if (website['Website name'] && website['Website URL']) {
+                    csvContent += `${website['Website name']},${website['Website URL']},${website['Login']},${website['Password']},${note},false\n`;
+                } else if (website['Application']) {
+                    csvContent += `${website['Application']},,${website['Login']},${website['Password']},${note},true\n`;
+                }
+            }
+            var blob = new Blob([csvContent], {type: 'text/csv'});
+            var url = URL.createObjectURL(blob);
+            var downloadLink = document.getElementById('downloadLink');
+            downloadLink.href = url;
+            downloadLink.download = 'kaspersky_export.csv';
+            downloadLink.click();
+        };
+        reader.readAsText(file);
+        }
+    )}
+}
+);
---
+++ kaspersky-txt-to-csv.py
@@ -0,0 +1,126 @@
+# Converts Kaspersky Password Manager export to Chromium Password Manager CSV export
+
+# Permission is granted to anyone to use this software for any purpose, including commercial applications, providing that the following conditions are met:
+# 1. Give appropriate credit, provide a link to the original source (https://github.com/Draggie306/kaspersky-to-csv) and indicate if changes were made.
+# 2. Do not use this software for illegal purposes.
+# 3. If you find a bug, report it to me so I can fix it.
+# 4. If you make any changes, you must release them under the same license (i.e. include these comment blocks in an unobfuscated form in the source code).
+# 5. Do not claim this software as your own.
+# 6. Have fun!
+
+import csv
+import os
+import time
+
+
+def process_text_file(text_file_path):
+    # get directoy path
+    """Example text file:
+    Websites
+
+    Website name: *****
+    Website URL: https://*****.co.uk
+    Login name: 
+    Login: *****
+    Password: *****
+    Comment: 
+
+    ---
+
+    Website name: *****
+    Website URL: https://*****.com
+    Login name: 
+    Login: *****@gmail.com
+    Password:*****
+    Comment: 
+
+    ---
+
+    REST OF WEBISTES
+
+    ---
+
+    Applications
+
+    Application: Kaspersky Password Manager
+    Login name: 
+    Login: a*****
+    Password: *****
+    Comment: 
+
+    ---
+
+    Application: Os maps
+    Login name: 
+    Login: *****@ibaguette.com
+    Password: *****
+    Comment: 
+
+    ---
+    
+    """
+
+    directory = os.path.dirname(text_file_path)
+    websites = []
+
+    with open(text_file_path, 'r') as file:
+        lines = file.readlines()
+
+    i = 0
+    while i < len(lines):
+        line = lines[i].strip()
+
+        if 'Websites' in line:
+            i += 1
+            while i < len(lines) and 'Applications' not in lines[i]:
+                if '---' in lines[i]:
+                    i += 1
+                    continue
+                record = {}
+                while i < len(lines) and '---' not in lines[i]:
+                    if 'Website name:' in lines[i]:
+                        record['Website name'] = lines[i].split(':')[1].strip()
+                    elif 'Website URL:' in lines[i]:
+                        fixed_url = (lines[i].split('Website URL: ')[1]).strip()
+                        record['Website URL'] = fixed_url
+                    elif 'Login:' in lines[i]:
+                        record['Login'] = lines[i].split(':')[1].strip()
+                    elif 'Password:' in lines[i]:
+                        record['Password'] = lines[i].split(':')[1].strip()
+
+                    # application records have no 'Website name' or 'Website URL' fields, just 'Application', 'Login Name', Login', 'Password', 'Comment'
+                    elif 'Application:' in lines[i]:
+                        record['Application'] = lines[i].split(':')[1].strip()
+                    elif 'Login:' in lines[i]:
+                        record['Login'] = lines[i].split(':')[1].strip()
+                    elif 'Password:' in lines[i]:
+                        record['Password'] = lines[i].split(':')[1].strip()
+                    elif 'Comment:' in lines[i]:
+                        record['Comment'] = lines[i].split(':')[1].strip()
+                    i += 1
+                print((f"Added {record['Website name']} (account #{len(websites) + 1})") if 'Website name' in record else 'Non-website record added')
+                websites.append(record)
+
+        print(f"Found {len(websites)} websites")
+
+    unix_time_seconds = int(time.time())
+    with open(os.path.join(directory, f'kaspersky_export_{unix_time_seconds}.csv'), 'w', newline='') as file:
+        writer = csv.writer(file)
+        # ...
+        writer.writerow(['name', 'url', 'username', 'password', 'note', 'application', 'comment'])
+
+        for account in websites:
+            if 'Website name' in account and 'Website URL' in account:
+                # This is a website record
+                writer.writerow([account['Website name'], account['Website URL'], account['Login'], account['Password'], ''])
+            elif 'Application' in account:
+                # This is an application record
+                writer.writerow([account['Application'], '', account['Login'], account['Password'], '', account['Comment']])
+
+    print(f'CSV file \'kaspersky_export_{unix_time_seconds}.csv\' created successfully!')
+    os.startfile(os.path.join(directory, f'kaspersky_export_{unix_time_seconds}.csv'))
+
+
+text_file_path = input('Enter the path of the text file: ')
+# text_file_path = "Z:\\23-12-2023.txt"
+process_text_file(text_file_path)
