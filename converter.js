f4071cc254b2512de8cf9a9347f35a41188e3164
Draggie <75524491+Draggie306@users.noreply.github.com>
2023-12-25 16:51:02 +0000

    Update button styles and add footer

--- converter.js
+++ converter.js
@@ -1,4 +1,4 @@
-// Last modified: 25/12/2023 16:27 by Draggie306
+// Last modified: 25/12/2023 16:50 by Draggie306
 // Converts Kaspersky Password Manager export to Chromium Password Manager CSV export
 
 // Permission is granted to anyone to use this software for any purpose, including commercial applications, providing that the following conditions are met:
@@ -17,77 +17,91 @@ document.addEventListener('DOMContentLoaded', function() {
     var convertButton = document.getElementById('convertButton');
     if (convertButton) {
         convertButton.addEventListener('click', function() {
-        var accountCount = 0;
-        var countOfAccounts = document.getElementById('countOfAccounts');
-        var fileInput = document.getElementById('fileInput');
-        var file = fileInput.files[0];
-        var reader = new FileReader();
-        reader.onload = function(e) {
-            var lines = e.target.result.split('\n');
-            var websites = [];
-            var i = 0;
-            while (i < lines.length) {
-                var line = lines[i].trim();
-                if (line.includes('Websites')) {
-                    i++;
-                    while (i < lines.length && !lines[i].includes('Applications')) {
-                        if (lines[i].includes('---')) {
-                            i++;
-                            continue;
-                        }
-                        var record = {};
-                        while (i < lines.length && !lines[i].includes('---')) {
-                            // holy this is ugly
-                            if (lines[i].includes('Website name:')) {
-                                let value = lines[i].split(':', 2)[1].trim();
-                                record['Website name'] = `"${value.replace(/"/g, '""')}"`;
-                            } else if (lines[i].includes('Website URL:')) {
-                                let fixed_url = lines[i].replace('Website URL: ', '').trim();
-                                if (!fixed_url.startsWith('http')) {
-                                    fixed_url = 'https://' + fixed_url;
-                                }
-                                record['Website URL'] = `"${fixed_url}"`;
-                                // record['Website URL'] = `"${lines[i].replace('Website URL: ', '').trim()}"`;
-                            } else if (lines[i].includes('Login:')) {
-                                let value = lines[i].split(':', 2)[1].trim();
-                                record['Login'] = `"${value.replace(/"/g, '""')}"`;
-                            } else if (lines[i].includes('Password:')) {
-                                let value = lines[i].split(':').slice(1).join(':').trim();
-                                record['Password'] = `"${value.replace(/"/g, '""')}"`;
-                            } else if (lines[i].includes('Application:')) {
-                                let value = lines[i].split(':', 2)[1].trim();
-                                record['Application'] = `"${value.replace(/"/g, '""')}"`;
-                            } else if (lines[i].includes('Comment:')) {
-                                let value = lines[i].split(':', 2)[1].trim();
-                                record['Comment'] = `"${value.replace(/"/g, '""')}"`;
+            var time_ms_start = Date.now();
+            // This looks much worse compared to the Python version.
+            // The CSV is generated in-memory and then downloaded.
+            // I experienced some issues with string manipulation edge cases with this version below, but I think they're all fixed now.
+            // that's why it looks messy, the string manipulation fixes these edge cases.
+            var accountCount = 0;
+            var countOfAccounts = document.getElementById('countOfAccounts');
+            var fileInput = document.getElementById('fileInput');
+            try {
+                var file = fileInput.files[0];
+            } catch (e) {
+                document.getElementById('Status').innerHTML = 'Status: No file selected';
+                return;
+            }
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var lines = e.target.result.split('\n');
+                var websites = [];
+                var i = 0;
+                while (i < lines.length) {
+                    var line = lines[i].trim();
+                    if (line.includes('Websites')) {
+                        i++;
+                        while (i < lines.length && !lines[i].includes('Applications')) {
+                            if (lines[i].includes('---')) { // dashes mark new account
+                                i++;
+                                continue;
                             }
-                            i++;
+                            var record = {};
+                            while (i < lines.length && !lines[i].includes('---')) {
+                                // holy this is ugly
+                                if (lines[i].includes('Website name:')) {
+                                    let value = lines[i].split(':', 2)[1].trim();
+                                    record['Website name'] = `"${value.replace(/"/g, '""')}"`;
+                                } else if (lines[i].includes('Website URL:')) {
+                                    let fixed_url = lines[i].replace('Website URL: ', '').trim();
+                                    if (!fixed_url.startsWith('http')) {
+                                        fixed_url = 'https://' + fixed_url;
+                                    }
+                                    record['Website URL'] = `"${fixed_url}"`;
+                                    // record['Website URL'] = `"${lines[i].replace('Website URL: ', '').trim()}"`;
+                                } else if (lines[i].includes('Login:')) {
+                                    let value = lines[i].split(':', 2)[1].trim();
+                                    record['Login'] = `"${value.replace(/"/g, '""')}"`;
+                                } else if (lines[i].includes('Password:')) {
+                                    let value = lines[i].split(':').slice(1).join(':').trim();
+                                    record['Password'] = `"${value.replace(/"/g, '""')}"`;
+                                } else if (lines[i].includes('Application:')) {
+                                    let value = lines[i].split(':', 2)[1].trim();
+                                    record['Application'] = `"${value.replace(/"/g, '""')}"`;
+                                } else if (lines[i].includes('Comment:')) {
+                                    let value = lines[i].split(':', 2)[1].trim();
+                                    record['Comment'] = `"${value.replace(/"/g, '""')}"`;
+                                }
+                                i++;
+                            }
+                            accountCount++;
+                            countOfAccounts.innerHTML = `Accounts found: ${accountCount}`;
+                            websites.push(record);
                         }
-                        accountCount++;
-                        countOfAccounts.innerHTML = `Accounts found: ${accountCount}`;
-                        websites.push(record);
+                    }
+                    i++;
+                }
+                var csvContent = 'name,url,username,password,note,isApplication\n';
+                for (var website of websites) {
+                    var note = website['Comment'] || '';
+                    if (website['Website name'] && website['Website URL']) {
+                        csvContent += `${website['Website name']},${website['Website URL']},${website['Login']},${website['Password']},${note},false\n`;
+                    } else if (website['Application']) {
+                        csvContent += `${website['Application']},,${website['Login']},${website['Password']},${note},true\n`;
                     }
                 }
-                i++;
+                var time_ms_end = Date.now();
+                var time_ms = time_ms_end - time_ms_start;
+                console.log(`Conversion took ${time_ms}ms`);
+                countOfAccounts.innerHTML = `Action completed for ${accountCount} accounts in ${time_ms}ms`;
+                var blob = new Blob([csvContent], {type: 'text/csv'});
+                var url = URL.createObjectURL(blob);
+                var downloadLink = document.getElementById('downloadLink');
+                downloadLink.href = url;
+                downloadLink.download = 'kaspersky_export.csv';
+                downloadLink.click();
+            };
+            reader.readAsText(file);
             }
-            var csvContent = 'name,url,username,password,note,isApplication\n';
-            for (var website of websites) {
-                var note = website['Comment'] || '';
-                if (website['Website name'] && website['Website URL']) {
-                    csvContent += `${website['Website name']},${website['Website URL']},${website['Login']},${website['Password']},${note},false\n`;
-                } else if (website['Application']) {
-                    csvContent += `${website['Application']},,${website['Login']},${website['Password']},${note},true\n`;
-                }
-            }
-            var blob = new Blob([csvContent], {type: 'text/csv'});
-            var url = URL.createObjectURL(blob);
-            var downloadLink = document.getElementById('downloadLink');
-            downloadLink.href = url;
-            downloadLink.download = 'kaspersky_export.csv';
-            downloadLink.click();
-        };
-        reader.readAsText(file);
-        }
-    )}
-}
+        )}
+    }
 );
